1cf9c701c6c29d8aed528a188d84f53d21e11f6e
yjcn <460990832@qq.com>
2018-06-06 17:20:38 +0800

    use pm2

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 
 app.get('/', function (req, res) {
-  res.send('Hello World!!!!')
+  res.send('<h1>XJTU SE</h1><img src="https://gss1.bdstatic.com/-vo3dSag_xI4khGkpoWK1HF6hhy/baike/c0%3Dbaike80%2C5%2C5%2C80%2C26/sign=468864cdf0039245b5b8e95de6fdcfa7/54fbb2fb43166d22ca0c87944a2309f79052d2b3.jpg">')
 })
 
 app.listen(3000, function () {
